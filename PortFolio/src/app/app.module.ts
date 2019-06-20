e8d3c00ada65976cc00a7bd511727b8beb6e50fe
Akash Anand <Akash@genprosys.com>
2019-06-20 14:48:24 -0400

    instructions added

--- PortFolio/src/app/app.module.ts
+++ PortFolio/src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// angular material imports
 import {MatCardModule} from '@angular/material/card';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
+//components
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
-    MatCardModule
+    MatCardModule,
+    MatSidenavModule,
+    BrowserAnimationsModule
 
   ],
   providers: [],

9cf15562b6397e2dc6fe3c5b2b64e9fe8bac1798
Akash Anand <36139806+akash9920@users.noreply.github.com>
2019-06-24 00:30:09 -0400

    nav bar

--- PortFolio/src/app/app.module.ts
+++ PortFolio/src/app/app.module.ts
@@ -5,24 +5,33 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // angular material imports
 import {MatCardModule} from '@angular/material/card';
 import {MatSidenavModule} from '@angular/material/sidenav';
-
+import { MatToolbarModule, MatButtonModule, MatIconModule, MatListModule } from '@angular/material';
+import { LayoutModule } from '@angular/cdk/layout';
 //components
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { MenuSideNavComponent } from './menu-side-nav/menu-side-nav.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    MenuSideNavComponent
   ],
   imports: [
     BrowserModule,
     MatCardModule,
     MatSidenavModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule
 
   ],
   providers: [],
